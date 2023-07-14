e3879a393780803bb5864d2d5840c65914f1ccbb
Justin <Justin>
2023-07-14 10:08:19 +0800

    upgrade to v2

--- blog/.vuepress/config.ts
+++ blog/.vuepress/config.ts
@@ -7,20 +7,36 @@ import sidebar from './config/sidebar';
 const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
+  /**
+   * 基础路径
+   */
   base: '/blog/',
 
+  /**
+   * 标题
+   */
   title: '雨停の精神時光屋',
 
-  description: '',
-
+  /**
+   * 在最终渲染出的 HTML 的 <head> 标签内加入的额外标签
+   */
   head: [
     ['link', { rel: 'icon', href: '/images/logo.svg' }],
   ],
 
+  /**
+   * 开发环境主机名
+   */
   host: '0.0.0.0',
 
+  /**
+   * 开发环境端口
+   */
   port: 8080,
 
+  /**
+   * 主题
+   */
   theme: defaultTheme({
     navbar,
 
